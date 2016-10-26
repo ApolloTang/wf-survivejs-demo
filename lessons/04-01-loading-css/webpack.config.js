731e490c443823954472d9a608cbc1bfcd45bcd0
Apollo Tang <tang.apollo@gmail.com>
2016-10-26 15:09:45 -0400

    added chapter 4.1 loading css

---
+++ lessons/04-01-loading-css/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+
+const parts = require('./libs/parts');
+
+
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+const common = {
+    entry: {
+        app: PATHS.app
+    },
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '01-00 Webpack demo'
+        })
+    ]
+};
+
+var config;
+// Detect how npm is run and branch based on that
+switch(process.env.npm_lifecycle_event) {
+    case 'build':
+        // config = merge(common, {});
+        config = merge(
+            common,
+            parts.setCSS(PATHS.app)
+        );
+    break;
+    default:
+        config = merge(
+            common,
+            parts.setupCSS(PATHS.app),
+            parts.devServer({
+                // Customize host/port here if needed
+                host: process.env.HOST,
+                port: process.env.PORT
+            })
+        )
+}
+
+module.exports = validate(config);
